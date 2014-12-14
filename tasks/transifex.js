8eeec57783a50d1e83676106f3f6dfbfc4b6c4c9
Joachim Jensen <jv@intox.dk>
2014-12-14 01:40:55 -0800

    add task tx_contributors to get list of contributors

--- tasks/transifex.js
+++ tasks/transifex.js
@@ -35,13 +35,41 @@ module.exports = function(grunt) {
       var api = new Transifex(self.options);
 
       async.waterfall([
-        api.availableResources,
+        api.resources,
         api.resourceDetails,
         api.languageStats,
         api.prepareRequests,
         api.fetchStrings,
         api.writeLanguageFiles
       ], done);
+    });
+  });
+
+grunt.registerMultiTask("tx_contributors", "Grunt that downloads string translations from Transifex", function() {
+    var self = this;
+
+    /* Extend given options with some defaults */
+    this.options = this.options({
+      languages: '*',
+      endpoint : 'http://www.transifex.com/api/2',
+      project  : this.target,
+      exclude : "intoxstudio".split(","),
+      mode: "json",
+      filename : "_resource_/_lang_.json",
+      templateFn: function(strings) { return JSON.stringify(_.object(_.pluck(strings, "key"), _.pluck(strings, "translation"))); }
+    });
+
+    /** Ensure we find some Transifex credentials
+     * then dos the main work */
+    var done = this.async();
+    credentials.read(function(err, creds) {
+      self.options.credentials = creds;
+      var api = new Transifex(self.options);
+
+      async.waterfall([
+        api.languages
+      ], done);
+      //api.languages();
     });
   });
 };
